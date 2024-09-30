82ff615a6c4ebc13eca5a249eaa99216bb65af03
asifalam515 <asibulalam515@gmail.com>
2024-09-30 15:37:15 +0600

    delete a specific user

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+  const handleDelete = (id) => {
+    // make sure user is confirm to delete
+    fetch(`http://localhost:5000/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("deleted");
+          console.log("deleted successfully");
+          //   remove the user from the user
+          const remainingUsers = users.filter((user) => user._id !== id);
+          setUsers(remainingUsers);
+        }
+      });
+  };
   return (
     <div>
       <div className="overflow-x-auto">
@@ -11,16 +30,25 @@ const Users = () => {
             <tr>
               <th>Email</th>
               <th>Created At</th>
+              <th>Last Logged In</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            {loadedUsers.map((user) => (
+            {users.map((user) => (
               <tr key={user._id}>
                 <td>{user?.email}</td>
                 <td>{user?.createdAt}</td>
-                <td>Delete</td>
+                <td> </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-error"
+                  >
+                    X
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
